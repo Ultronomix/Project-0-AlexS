9b49e5779a51a7a542dbfc85c8f8179d547f411c
Alex <asacaquirin17@gmail.com>
2022-08-22 22:52:36 -0400

    adding json

--- src/main/java/Controller/BikeController.java
+++ src/main/java/Controller/BikeController.java
@@ -1,4 +1,15 @@
 package Controller;
 
+import Models.Bike;
+
+import java.util.logging.Handler;
+
 public class BikeController {
+
+    private Handler createNewBike = ctx ->{
+        Bike newBike = ctx.bodyAsClass(Bike.class);
+        try {
+            ctx
+        }
+    }
 }
