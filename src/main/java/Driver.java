8e467a927285940bbcfe15ec030cffa93bb49bde
Alex <asacaquirin17@gmail.com>
2022-08-23 16:31:00 -0400

    some changes

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,5 +1,15 @@
+import io.javalin.Javalin;
+
 public class Driver {
     public static void main(String[]args){
         BikeController bikeController = new BikeController();
+
+        Javalin app = Javalin.create().start(8080);
+
+        app.get("/");
+        app.post("/");
+
+
+            }
+
     }
-}
