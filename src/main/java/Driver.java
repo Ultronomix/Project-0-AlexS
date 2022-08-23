5db3332354bcb3adf28836e3d5c76ce12a455422
Alex <asacaquirin17@gmail.com>
2022-08-22 22:39:56 -0400

    driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,5 +1,3 @@
-package com.revature;
-
 public class Driver {
     public static void main(String[]args){
         BikeController bikeController = new BikeController();
---
+++ src/main/java/Models/Bike.java
@@ -0,0 +1,72 @@
+package Models;
+
+import java.util.Objects;
+
+public class Bike {
+    private String name;
+    private String color;
+
+    private String year;
+
+    public String getName() {
+        return name;
+    }
+
+    public Bike(String name, String color, String year) {
+        this.name = name;
+        this.color = color;
+        this.year = year;
+    }
+
+    public Bike setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Bike setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public Bike setYear(String year) {
+        this.year = year;
+        return this;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bike bike = (Bike) o;
+        return name.equals(bike.name) && color.equals(bike.color) && year.equals(bike.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", year='" + year + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
+
