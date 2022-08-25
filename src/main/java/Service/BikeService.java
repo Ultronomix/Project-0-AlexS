506d52e6edc9e0a02041f98fbd9f6f7d0203f51b
Alex <asacaquirin17@gmail.com>
2022-08-24 21:23:05 -0400

    service done

--- src/main/java/Service/BikeService.java
+++ src/main/java/Service/BikeService.java
@@ -1,4 +1,17 @@
 package Service;
 
+import Models.Bike;
+import Repository.BikeRepository;
+
+import java.util.List;
+
 public class BikeService {
+    private final BikeRepository bikeRepository = new BikeRepository();
+
+    public Bike createBike(Bike bike){
+        return bikeRepository.create(bike);
+    }
+    public List<Bike> getAllBikes(){
+        return bikeRepository.getAll();
+    }
 }
